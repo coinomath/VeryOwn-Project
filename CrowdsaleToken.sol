c7d579eb800743850da0499462d2c872047642d1
coinomath <30578154+coinomath@users.noreply.github.com>
2017-08-01 23:57:02 +0300

    Update CrowdsaleToken.sol

--- CrowdsaleToken.sol
+++ CrowdsaleToken.sol
@@ -6,7 +6,7 @@ contract VeryOwnCoin is ERC20 {
 
 	//CONSTANTS
 	// Maximum number of VeryOwnCoin to sell
-	uint public constant _totalSupply = 240000000; // 240 000 000 VOC
+	uint public constant _totalSupply = 24000000; // 24 000 000 VOC
 	
 	string public constant symbol = "VOC";
 	string public constant name = "VeryOwn Coin";
@@ -21,50 +21,61 @@ contract VeryOwnCoin is ERC20 {
 	
 	function totalSupply() constant returns (uint256 totalSupply) {
 		return _totalSupply;
-	
 	}
 	
 	function balanceOf(address _owner) constant returns (uint256 balance) {
 		return balances[_owner];
-	
 	}
 	function transfer(address _to, uint256 _value) returns (bool success) {
 		require (
-		balances[msg.sender] >= _value
-		&& _value > 0
+			balances[msg.sender] >= _value
+			&& _value > 0
 		);
 		balances[msg.sender] -= _value;
 		balances[_to] += _value;
-		Transfer(meg.sender, _to, _value);
+		Transfer(msg.sender, _to, _value);
 		return true;
 	}
 	
 	function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+	require(
+		allowed [_from][msg.sender] >= _value
+		&& balancess [_from][msg.sender] >= _value
+		&& _value > 0
+	);
+	balances[msg.sender] -= _value;
+	balances[_to] += _value;
+	allowed[_from][msg.sender] -= _value;
+	Transfer(_from, _to, _value);
+	return true;
 	}
 	
+	function approve(address _spender, uint256 _value) returns (bool success) {
+		allowed[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+		return true;	
+	}
 	
-	
-	
-	
+	function allowance (address _owner, address _spender) constant returns (uint256 remaining)
+		return allowed[_owner][_spender];
+		
 	// Minimum amount to invest
-	uint public constant MIN_INVEST_ETHER = 100 finney; // 0.1ETH
+	uint public constant MIN_INVEST_ETHER = 50 finney; // 0.05 ETH
 	// Crowdsale period
-	uint private constant CROWDSALE_PERIOD = 22 days; // 22 days crowdsale run
-	// Number of TRIBE per Ether
-	uint public constant COIN_PER_ETHER = 3000000000; // 3 000 TRIBE
+	uint private constant CROWDSALE_PERIOD = 31 days; // 22 days crowdsale run
+	// Number of VOC per Ether
+	uint public constant COIN_PER_ETHER = 2000; // 2000 VOC
 
 
 	//VARIABLES
-	// TRIBE contract reference
+	// VOC contract reference
 	Token public coin;
-    // Multisig contract that will receive the Ether
-	address public multisigEther;
 	// Number of Ether received
 	uint public etherReceived;
-	// Number of TRIBE sent to Ether contributors
+	// Number of VOC sent to Ether contributors
 	uint public coinSentToEther;
-  // Number of TRIBE to burn
-  uint public coinToBurn;
+  	// Number of VOC to burn
+  	uint public coinToBurn;
 	// Crowdsale start time
 	uint public startTime;
 	// Crowdsale end time
@@ -93,121 +104,6 @@ contract VeryOwnCoin is ERC20 {
 	event LogReceivedETH(address addr, uint value);
 	event LogCoinsEmited(address indexed from, uint amount);
 
-	//Crowdsale Constructor
-	function Crowdsale(address _TRIBEAddress, address _to) {
-		coin = Token(_TRIBEAddress);
-		multisigEther = _to;
-	}
-	
-	// Default function to receive ether
-	function() stopInEmergency respectTimeFrame payable {
-		receiveETH(msg.sender);
-	}
 
-	 
-	// To call to start the crowdsale
-	function start() onlyOwner {
-		if (startTime != 0) throw; // Crowdsale was already started
-
-		startTime = now ;            
-		endTime =  now + CROWDSALE_PERIOD;    
-	}
-
-	// Main function on ETH receive
-	function receiveETH(address beneficiary) internal {
-		if (msg.value < MIN_INVEST_ETHER) throw; // Do not accept investment if the amount is lower than the minimum allowed investment
-		
-		uint coinToSend = bonus(msg.value.mul(COIN_PER_ETHER).div(1 ether)); // Calculate the amount of tokens to send
-		if (coinToSend.add(coinSentToEther) > MAX_CAP) throw;	
-
-		Backer backer = backers[beneficiary];
-		coin.transfer(beneficiary, coinToSend); // Transfer TRIBE
-
-		backer.coinSent = backer.coinSent.add(coinToSend);
-		backer.weiReceived = backer.weiReceived.add(msg.value); // Update the total wei collected during the crowdfunding for this backer    
-
-		etherReceived = etherReceived.add(msg.value); // Update the total wei collected during the crowdfunding
-		coinSentToEther = coinSentToEther.add(coinToSend);
-
-		// Send events
-		LogCoinsEmited(msg.sender ,coinToSend);
-		LogReceivedETH(beneficiary, etherReceived); 
-	}
-	
-
-	// Bonus function for the first week
-	function bonus(uint amount) internal constant returns (uint) {
-		if (now < startTime.add(7 days)) return amount.add(amount.div(5));   // bonus 20%
-		return amount;
-	}
-
-	// Finalize function
-	function finalize() onlyOwner public {
-
-        // Check if the crowdsale has ended or if the old tokens have been sold
-    if(coinSentToEther != MAX_CAP){
-        if (now < endTime)  throw; // If Crowdsale still running
-    }
-		
-		if (!multisigEther.send(this.balance)) throw; // Move the remaining Ether to the multisig address
-		
-		uint remains = coin.balanceOf(this);
-		if (remains > 0) {
-      coinToBurn = coinToBurn.add(remains);
-      // Transfer remains to owner to burn
-      coin.transfer(owner, remains);
-		}
-		crowdsaleClosed = true;
-	}
-
-	// Drain functions in case of unexpected issues with the smart contract.
-  // ETH drain
-	function drain() onlyOwner {
-    if (!multisigEther.send(this.balance)) throw; //Transfer to team multisig wallet
-	}
-  // TOKEN drain
-  function coinDrain() onlyOwner {
-    uint remains = coin.balanceOf(this);
-    coin.transfer(owner, remains); // Transfer to owner wallet
-	}
-
-	// Change multisig wallet in case its needed
-	function changeMultisig(address addr) onlyOwner public {
-		if (addr == address(0)) throw;
-		multisigEther = addr;
-	}
-
-	// Change contract ownership
-	function changeTribeOwner() onlyOwner public {
-		coin.transferOwnership(owner);
-	}
-
-	// Toggle refund state on and off
-	function setRefundState() onlyOwner public {
-		if(enableRefund == false){
-			enableRefund = true;
-		}else{
-			enableRefund = false;
-		}
-	}
-
-
-	//Refund function when minimum cap isnt reached, this is step is step 2, THIS FUNCTION ONLY AVAILABLE AFTER BEING ENABLED.
-	//STEP1: From TRIBE token contract use "approve" function with the amount of TRIBE you got in total.
-	//STEP2: From TRIBE crowdsale contract use "refund" function with the amount of TRIBE you got in total.
-	//STEP3: From TRIBE crowdsale contract use "withdrawPayement" function to recieve the ETH.
-	function refund(uint _value) refundStatus public {
-		
-		if (_value != backers[msg.sender].coinSent) throw; // compare value from backer balance
-
-		coin.transferFrom(msg.sender, address(this), _value); // get the token back to the crowdsale contract
-
-		uint ETHToSend = backers[msg.sender].weiReceived;
-		backers[msg.sender].weiReceived=0;
-
-		if (ETHToSend > 0) {
-			asyncSend(msg.sender, ETHToSend); // pull payment to get refund in ETH
-		}
-	}
 
 }
