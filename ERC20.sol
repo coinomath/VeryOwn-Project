83b76a2e6258fd81829d95ef3b3dd70af2c997fd
coinomath <30578154+coinomath@users.noreply.github.com>
2017-08-01 22:47:41 +0300

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,20 +1,11 @@
 pragma solidity ^0.4.11;
 
-contract ERC20Basic {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  function transfer(address to, uint value);
-  event Transfer(address indexed from, address indexed to, uint value);
+interface ERC20 {
+  function totalSupply() constant returns (uint256 totalSupply);
+  function balanceOf(address _owner) constant returns (uint256 balance);
+  function transfer(address _to, uint256 _value) returns (bool success);
+  function transfer(address _from, address _to, uint256 _value) returns (bool success);
+  function approve(address _spender, uint256 _value) returns (bool success);
+  event Transfer(address indexed _from, address indexed _to, uint256 _value); 
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
-
-contract ERC20 is ERC20Basic {
-  function allowance(address owner, address spender) constant returns (uint);
-  function transferFrom(address from, address to, uint value);
-  function approve(address spender, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-    Contact GitHub API Training Shop Blog About 
-
-    © 2017 GitHub, Inc. Terms Privacy Security Status Help 
-
